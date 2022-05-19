3af19a913cd372c7fa4c191a7345d4e46ea34195
stepanets-vadym <Alkatras427@gmail.com>
2022-05-20 00:15:44 +0300

    some fix

--- index.js
+++ index.js
@@ -1,21 +1,36 @@
 // Burger
 
 const burger = document.querySelector('.vad-header__burger');
-if (burger){
+if (burger) {
   const navigation = document.querySelector('.navigation');
-  burger.addEventListener("click", function(e){
+  burger.addEventListener('click', function (e) {
     document.body.classList.toggle('_lock');
     burger.classList.toggle('_active');
     navigation.classList.toggle('_active');
   });
 }
 
-window.onscroll = function() {scrollFunction()};
+window.onscroll = function () {
+  scrollFunction();
+};
 
 function scrollFunction() {
   if (document.body.scrollTop > 50 || document.documentElement.scrollTop > 50) {
-    document.getElementById("header").style.padding = "16px 10px";
+    document.getElementById('header').style.padding = '16px 10px';
   } else {
-    document.getElementById("header").style.padding = "40px 10px";
+    document.getElementById('header').style.padding = '40px 10px';
   }
 }
+
+// document.getElementById('js-reversal-card-1').addEventListener('click', () => {
+//   document.getElementById('js-reversal-card-1').classList.toggle('reversal');
+// });
+document.getElementById('js-reversal-card-2').addEventListener('click', () => {
+  document.getElementById('js-reversal-card-2').classList.toggle('reversal');
+});
+// document.getElementById('js-reversal-card-3').addEventListener('click', () => {
+//   document.getElementById('js-reversal-card-3').classList.toggle('reversal');
+// });
+document.getElementById('js-reversal-card-4').addEventListener('click', () => {
+  document.getElementById('js-reversal-card-4').classList.toggle('reversal');
+});
